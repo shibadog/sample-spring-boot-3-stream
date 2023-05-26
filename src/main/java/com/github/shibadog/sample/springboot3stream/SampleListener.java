cfed76d26a3fc14704ebfd0a82e42144a472fa77
shibadog <shiba_dog@outlook.com>
2023-05-26 04:14:33 +0000

    filter処理を行うための手続き

--- src/main/java/com/github/shibadog/sample/springboot3stream/SampleListener.java
+++ src/main/java/com/github/shibadog/sample/springboot3stream/SampleListener.java
@@ -11,7 +11,10 @@ import org.springframework.cloud.function.context.DefaultMessageRoutingHandler;
 import org.springframework.cloud.function.context.MessageRoutingCallback;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.GlobalChannelInterceptor;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -64,6 +67,28 @@ public class SampleListener {
             public void accept(Message<?> message) {
                 log.info("no routing header: {}", message.getHeaders());
                 log.info("no routing receive message!! {}", message.getPayload());
+            }
+        };
+    }
+
+    @Bean
+    @GlobalChannelInterceptor(patterns = "input") // bindingNameのほうを指定する必要がある！
+    ChannelInterceptor customInterceptor() {
+        return new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                log.info("preSend channel:{}", channel.toString());
+                return ChannelInterceptor.super.preSend(message, channel);
+            }
+            @Override
+            public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+                log.info("postSend channel:{}", channel.toString());
+                ChannelInterceptor.super.postSend(message, channel, sent);
+            }
+            @Override
+            public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
+                log.info("afterSendCompletion channel:{}", channel.toString());
+                ChannelInterceptor.super.afterSendCompletion(message, channel, sent, ex);
             }
         };
     }
